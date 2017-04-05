7e709381197ce927b1380e26a4f22a4581b7bf83
Renaud <PsYc0p4tH@gmail.com>
2017-04-05 15:28:59 -0400

    question 3

--- euleur.rb
+++ euleur.rb
@@ -7,3 +7,37 @@ end
 puts arr.select(&:even?).inject(:+)
 
 #Number 3
+
+require 'prime'
+600851475143.prime_division
+
+#or
+
+def factors(num)
+  factors=[]
+  for i in 1 .. num
+    if num % i == 0
+      if factors.include? num / i
+        break
+      end
+      factors << i
+    end
+  end
+  return factors
+end
+
+
+def isPrime?(num)
+  for i in 2 ... num do
+    return false if num % i == 0
+  end
+  true
+end
+
+test = []
+for i in factors(600851475143)
+  test << i if isPrime?(i)
+end
+puts test.max
+
+
